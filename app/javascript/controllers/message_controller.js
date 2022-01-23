91f56039668eada5440a12f82233bfd9cc86fcf8
Armand Fardeau <fardeauarmand@gmail.com>
2022-01-23 02:38:33 +0100

    Add tailwind and template

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -2,28 +2,36 @@ import {Controller} from "@hotwired/stimulus"
 import {createConsumer} from "@rails/actioncable"
 
 export default class extends Controller {
-    static targets = ["content", "room"]
+    static targets = ["content", "room", "user"]
 
     connect() {
         this.subscribe(this.room);
+        this.user = this.userTarget.textContent
     }
 
     get messageList() {
         return document.getElementById("message-list")
     }
 
+    get template(){
+        return document.getElementById("message-template")
+    }
 
     clearMessages() {
         this.messageList.innerHTML = "";
     }
 
     addMessage(data) {
-        let messageWrapper = document.createElement('li')
-        let messageItem = document.createElement('p')
-        messageWrapper.append(messageItem);
-        messageItem.append(`sent by: ${data.sent_by} ${data.text}`);
+        let clone = this.template.content.cloneNode(true);
+        let message = clone.querySelectorAll("p")[0];
+        let author = clone.querySelectorAll("span")[0];
+        let listElement = clone.querySelectorAll("li")[0];
 
-        this.messageList.append(messageItem);
+        message.innerText = data.text;
+        author.innerText = data.sent_by;
+        listElement.classList.add(data.sent_by == this.user ? listElement.dataset.sentBySelf : listElement.dataset.sentByOther);
+
+        this.messageList.appendChild(clone);
     }
 
     beforeSubscriptionChange() {
@@ -53,7 +61,7 @@ export default class extends Controller {
     }
 
     send() {
-        if(!this.contentTarget.value.trim()) return;
+        if (!this.contentTarget.value.trim()) return;
 
         this.chat.perform('send_message', {text: this.contentTarget.value})
         this.contentTarget.value = '';
