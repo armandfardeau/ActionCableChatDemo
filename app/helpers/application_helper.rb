db18ee59346e3371092984eb8e8ee6db89753278
Armand Fardeau <fardeauarmand@gmail.com>
2022-01-23 19:24:28 +0100

    Use helper to render css class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
+require "ostruct"
+
 module ApplicationHelper
+  def message_style
+    @message_style ||= OpenStruct.new(
+      span: OpenStruct.new(
+        css_class: "italic font-bold text-xs"
+      ),
+      li: OpenStruct.new(
+        css_class: "flex flex-col mt-5",
+        data: OpenStruct.new(
+          sent_by_self: "items-start",
+          sent_by_other: "items-end",)
+      )
+    )
+  end
 end
